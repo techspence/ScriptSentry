a32c909219604ecc4696ea001b0b65a50d6b77c9
techspence <alessisb@gmail.com>
2023-08-08 04:36:08 -0700

    added rebelinux pr to scriptsentry standalone script

--- ScriptSentry.ps1
+++ ScriptSentry.ps1
@@ -105,7 +105,7 @@ function Find-LogonScriptCredentials {
     )
     foreach ($script in $LogonScripts) {
         # Write-Verbose -Message "Checking $($Script.FullName) for credentials.."
-        $Credentials = Get-Content -Path $script.FullName | Select-String -Pattern "/user:" -AllMatches
+        $Credentials = Get-Content -Path $script.FullName | Select-String -Pattern "/user:","-AsPlainText" -AllMatches
         if ($Credentials) {
             # "`n[!] CREDENTIALS FOUND!"
             $Credentials | ForEach-Object {
